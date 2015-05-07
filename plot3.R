11161e7e808cf60a3a0a094b2e3ec7ab19dbf3af
onthrak <kcprgoc@gmail.com>
2015-05-07 08:36:38 +0200

    new

--- plot3.R
+++ plot3.R
@@ -12,6 +12,14 @@ lines(Sub_metering_2, col="red")
 lines(Sub_metering_3, col="blue")
 axis(1, at=c(1,1440,2879), labels=c("Thu","Fri","Sat"))
 legend("topright",lty=1, col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.copy(png,filename="plot3.png", width=480, height=480);
+dev.copy(png,filename="plot3.png", width=480, height=480 );
 dev.off ()
-png(filename = "plot3.png",width = 480, height = 480, units = "px", pointsize = 12, bg = "white") 
+#### z png
+png(file="plot3.png",width=480,height=480)
+plot(Sub_metering_1, type="l", xaxt = "n", ylab="Energy sub metering" ,xlab="")
+lines(Sub_metering_2, col="red")
+lines(Sub_metering_3, col="blue")
+axis(1, at=c(1,1440,2879), labels=c("Thu","Fri","Sat"))
+legend("topright",lty=1, col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
